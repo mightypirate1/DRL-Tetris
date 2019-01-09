d4e8238075946629c7e20e7387d6f7b0a9f83113
Martin Frisk <yfflan@gmail.com>
2019-01-09 23:06:48 +0100

    Test script for multithreading

--- speedtest_thread.py
+++ speedtest_thread.py
@@ -1,7 +1,7 @@
 from environment.tetris_environment_vector import tetris_environment_vector
 from environment.tetris_environment import tetris_environment
 from agents.vector_agent.vector_agent import vector_agent
-import threaded_runner
+import threads.threaded_runner
 
 import tensorflow as tf
 import docopt
@@ -14,7 +14,7 @@ n_envs  = 64
 docoptstring = \
 '''Speedcheck!
 Usage:
-  Speedcheck.py [--n N] [--m M] [--steps S]  [--no-rendering]
+  Speedcheck.py [--n N] [--m M] [--steps S]  [--no-rendering] [--debug]
 
 Options:
     --n N      N envs per thread. [default: 16]
@@ -27,16 +27,22 @@ n_workers = int(docoptsettings["--m"])
 n_envs_per_thread = int(docoptsettings["--n"])
 n_envs = n_workers * n_envs_per_thread
 render = not docoptsettings["--no-rendering"]
+debug = docoptsettings["--debug"]
 
 settings = {
-            "worker_steps"      : total_steps // n_envs,
-            "n_envs_per_thread" : n_envs_per_thread,
-            "render"            : render,
-            "n_workers"         : n_workers,
+            #Types
             "env_vector_type"   : tetris_environment_vector,
             "env_type"          : tetris_environment,
             "agent_type"        : vector_agent,
-            "process_patience"  : [0.1,0.1, 0.1],
+            #Threading
+            "worker_batch_size" : 128,
+            "n_workers"         : n_workers,
+            "n_envs_per_thread" : n_envs_per_thread,
+            "worker_steps"      : total_steps // n_envs,
+            "process_patience"  : [0.1,0.1, 0.1], #runner/trainer/pool
+            "worker_net_on_cpu" : True,
+            #Misc.
+            "render"            : render,
            }
 
 print("Speedcheck:")
@@ -44,11 +50,20 @@ for x in docoptsettings:
     print("\t{} : {}".format(x,docoptsettings[x]))
 
 
+pool = threads.threaded_runner.threaded_runner(
+                                                settings=settings
+                                              )
+
+##
+#Thread debugger
+#We get better error messages if we run just one process. Activate with "--debug"
+if debug:
+    print("Executing only thread_0:")
+    pool.threads[0]()
+    exit("___")
+
 #Init PARALLEL
 ###########################
-pool = threaded_runner.threaded_runner(
-                                        settings=settings
-                                      )
 print("Starting pool")
 pool.run(total_steps // n_envs )
 pool.join()
@@ -58,7 +73,6 @@ print("Pool finished")
 sys.stdout.flush()
 T_parallel = pool.sum_return_que() / n_workers
 ###########################
-
 
 #
 ##Init SERIAL
@@ -75,10 +89,6 @@ with tf.Session() as session:
                                    session=session,
                                    settings=settings,
                                   )
-
-    session.run(tf.global_variables_initializer())
-    agent.reinitialize_model()
-
     print("Starting serial")
     T_serial_start = time.time()
     current_player = 1
@@ -88,10 +98,10 @@ with tf.Session() as session:
         current_player = 1 - current_player
         _,a = agent.get_action(s, player=current_player)
         # a = env.get_random_action()
-        ds = env.perform_action(a)
+        r, ds = env.perform_action(a, player=current_player)
         s = env.get_state()
         for i,d in enumerate(ds):
-            if d: env.reset(env=[i])
+            if d: env.reset(env=i)
         if render:
             env.render(env=0)
         print("Step {}".format((t+1)))
@@ -102,6 +112,5 @@ with tf.Session() as session:
 #Show what we collected (parallel)!
 print("[Parallel] Collected {} trajectories:".format(None))
 print("Total: {} collected in {} seconds ({} steps/second)".format(total_steps, T_parallel, total_steps/T_parallel))
-
 print("[Serial] Collected {} trajectories:".format(None))
 print("Total: {} collected in {} seconds ({} steps/second)".format(total_steps, T_serial_stop - T_serial_start, total_steps/(T_serial_stop - T_serial_start) ))
