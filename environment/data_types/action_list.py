22e358c5733182263b3cde861e50c8727ed86938
Martin Frisk <yfflan@gmail.com>
2019-01-10 20:42:13 +0100

    action_list uses action class

--- environment/data_types/action_list.py
+++ environment/data_types/action_list.py
@@ -1,21 +1,23 @@
+from environment.data_types.action import action, null_action
+
 class action_list(list):
     def __init__(self, container=None, remove_null=False):
         self.remove_null = remove_null
         if container is None or len(container) == 0:
-            container = [[0]]
-        if [0] not in container:
-            self.container = [[0]]
+            container = [null_action]
+        if null_action not in container:
+            self.container = [null_action]
         else:
             self.container = []
         for x in container:
             assert type(x) is list, "attempted to create action list with non-list type actions (type={}). An actions list is of the form [s_1,...,s_n] where each s_i is a list of actions [a_1,...,a_m], and each a_j an integer.".format(type(x))
             if x not in self.container:
-                self.container.append(x)
+                self.container.append(action(x))
         if self.remove_null:
             self.remove_nulls()
     def remove_nulls(self):
-        while len(self.container) > 1 and [0] in self.container:
-            self.container.remove([0])
+        while len(self.container) > 1 and null_action in container:
+            self.container.remove(null_action)
     def __add__(self,n):
         return action_list(n.container+self.container, remove_null=(n.remove_null or self.remove_null))
     def __str__(self):
@@ -25,7 +27,7 @@ class action_list(list):
     def __getitem__(self, key):
         if key > len(self.container):
             print("I HOPE THAT ACTION WAS NOT NECESSARY......")
-            return [0]
+            return null_action
         return self.container[key]
     def __len__(self):
         return len(self.container)
