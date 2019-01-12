b12a305ee9695bfd66a66221f22edb1b9f879942
Martin Frisk <yfflan@gmail.com>
2019-01-12 01:47:44 +0100

    major rework of experience replay. should be much faster, and much much tidier

---
+++ agents/agent_utils/data_entry.py
@@ -0,0 +1,36 @@
+class data_entry: #Wrapper class for entries, so that sort algorithms can get handy comparisons
+    def __init__(self, data, prio, time_stamp):
+        self.data = data
+        self.prio = prio
+        self.time_stamp = time_stamp
+        self.is_weight = None
+    def set_prio(self, p):
+        self.prio = p
+    def set_isw(self, w):
+        self.is_weight = w
+    def set_time(self,t):
+        self.time_stamp = t
+    def __getvalue__(self):
+        return self.data
+    def __lt__(self, e):
+        if type(e) is data_entry:
+            e = e.prio
+        return e > self.prio
+    def __le__(self, e):
+        if type(e) is data_entry:
+            e = e.prio
+        return e >= self.prio
+    def __gt__(self, e):
+        if type(e) is data_entry:
+            e = e.prio
+        return self.prio > e
+    def __ge__(self, e):
+        if type(e) is data_entry:
+            e = e.prio
+        return self.prio >= e
+    def __eq__(self, e):
+        if type(e) is data_entry:
+            e = e.prio
+        return self.prio == e
+    def __str__(self):
+        return "data_entry(prio:{}, time:{}, isw:{}, data:{})".format(self.prio, self.time_stamp, self.is_weight, self.data)
--- agents/agent_utils/experience_replay.py
+++ agents/agent_utils/experience_replay.py
@@ -1,94 +1,53 @@
-from collections import deque
-from math import ceil
-import bisect
 import numpy as np
+from math import ceil
+from heapq import merge
+import bisect
 import aux.utils as utils
+from .. import agent_utils
 
 '''
-This class is made to store experiences (arbitrary data).
+This class is made to store experiences (arbitrary data basically).
 It is a simple implementation of https://arxiv.org/abs/1511.05952 and hopefully
 correct and efficient :)
 
-DESCRIPTION:
-experience_replay is made to use prioritization and timestamps although both can
-be turned off. (prioritization is turned off in the constructor but you still
-need to pass some bogus number to the methods if the signature so demands.
-time-stamps are "turned off" by always passing time_stamps=0 to add_samples.)
+You feed it lists with data, priorities and optional timestamp.
+It creates a data_entry type object that it keeps sorted based on priority.
 
-The class stores data, and allows for manipulation of it with the following
-methods:
+You can get a random sample (of n data_entry objects) with get_random_sample(n).
+The caller "promises" to replace them later. The caller is responsible for
+updating the priorities before replacing them if that is part of the algorithm.
 
-get_random_sample(n):
-    short description: returns a random sample WITH replacement of n. It's
-    possible to change alpha, gamma (see paper for details).
+Since the sampling is with replacement, get_random_sample also provides a filter.
+Just save the filter, and pass it to replace_samples when you replace them (after
+having updated the prio).
 
-    longer description:
-    the goal is to supply a random sample of size n, containing:
-        data,
-        priorities,
-        importance sampling weights,
-        time-stamps
+Most importand fcns to know:
+get_random_sample(n)
+    gives out a random (with replacement!) sample of data_entry objects.
 
-    since probably want to replace the samples after updating the priorities, we
-    use a filter which is thus also returned. The purpose of the filter is that
-    the caller of get_random_sample updates the priorities, and then add back
-    ALL samples together with the accompanying meta-data (prio/time-stamps) AND
-    also passing the filter. This makes it so that we only add back samples that
-    were sampled multiple times (replacement, remember..) are only added back
-    once.
+add_samples(data,prio, time_stamps=None)
+    if you have collected fresh data from the env. use this to get it in the buffer.
 
-    the full return is thus: return data,prio,time-stamps,is-weights,filter
+replace_samples(data, filter)
+    if you got a sample from get_random_sample it came with a filter. pass that
+    sample (update the priority value first if you plan to do that) together with
+    the filter to replace them properly in the buffer.
 
-add_samples(data,prio,time_stamps=None, filter=None):
-    This adds lists of samples to the data, while maintaining the list sorted
-    ascendingly in priority.
-
-    If no time stamps are specified, the buffer keeps a
-    clock, and uses that for time-stamps. You can also give a filter (i.e. a
-    list of indices) which if specified, discards all the samples not in the
-    filter. This is to make replacement easier, since the caller does not need
-    to figure out.
-
-    Use-case 1: I want to add a new sample (e.g.fresh from the environment)!
-        Just call
-        ´´´
-            experience_replay.add_samples(data, prio)
-        ´´´
-        to add a newly gathered data-point
-
-    Use-case 2: I want to replace data that the experience_replay previously
-    gave me!
-        instead call:
-            experience_replay.add_samples(
-                                          data,
-                                          updated_prio,
-                                          time_stamps=time_stamps,
-                                          filter=filter,
-                                         )
-    to replace a (data,prio,time_stamps,is_weights,filter)-tuple received from
-    the get_random_sample method.
-
-merge(experience_replay)
-    merges self with experience_replay.
+merge_in(another_buffer)
+    merges the data of another_buffer into this buffer, maintaining sorted order.
 '''
-
 class experience_replay:
     def __init__(self, size, prioritized=True, log=None):
         self.test_flag = False
         self.max_size = size
         self.log = log
-        self.data = deque()
-        self.prio = deque()
-        self.time_stamps = deque()
+        self.data = list()
         self.time = 0
         self.prioritized = prioritized
-
     def get_random_sample(self, n_samples, alpha=1.0, beta=1.0, remove_samples=True):
-        ##Random sampling of indices, with replacement and custom-distribution
+        #Create the sampling distribution (see paper for details)
         n = len(self)
         all_indices = np.arange(n)
-
-        #Create the sampling distribution (see paper for details)
         if self.prioritized:
             one_over_n = 1/n #Rank-based sampling
             one_over_surpriserank = (1/(n-all_indices))
@@ -106,13 +65,11 @@ class experience_replay:
         indices = np.random.choice(all_indices, size=n_samples, p=p).tolist()
 
         ##Data collection, and index-tracking
-        sample_data, sample_prio, sample_time_stamps = [None]*n_samples, [None]*n_samples, [None]*n_samples
+        sample_data = [None]*n_samples
         for i, idx in enumerate(indices):
             idx_dict[idx]         = i #This makes sure that idx_dict keeps track of the position of the last occurance of each index in the sample
             sample_data[i]        = self.data[idx]
-            sample_prio[i]        = self.prio[idx]
-            sample_time_stamps[i] = self.time_stamps[idx]
-        sample_is_weights = is_weights_all[indices,:]
+            sample_data[i].set_isw(is_weights_all[idx])
         filter = list(idx_dict.values()) #The filter is a list containing the last occurance of each unique sample. This means, that if we add back as many samples as is in the filter, the number of samples in the buffer is unchanged.
 
         #If the caller did not specify that they will not give the samples back (by setting remove_samples=False), we assume they want to update priority etc. Since we do not have an update-prio fcn (this may be on the wish-list) we instead just remove the samples, and expect the caller to replace them later
@@ -121,13 +78,11 @@ class experience_replay:
             indices_to_be_removed.sort(reverse=True)
             for idx in indices_to_be_removed:
                 del self.data[idx]
-                del self.prio[idx]
-                del self.time_stamps[idx]
 
-        #We now give the sampled data,prio,time-stamps, is-weights to the caller.
-        #The filter is provided, so that they can compute new priorities, and pass those back to us through the add_samples methodself.
-        #IMPORTANT: pass the filter given here to the add_samples method when replacing samples, otherwise any data that occured n times in the sample given out, will be added n times back, which is NOT how you want to do it most likely...
-        return sample_data, sample_prio, sample_time_stamps, sample_is_weights, filter
+        #We now give the sampled data to the caller.
+        #The filter is provided so that they can compute new priorities, update the data_entries, and put them back through replace_samples.
+        #IMPORTANT: pass the filter given here to the replace_samples method when replacing samples, otherwise any data that occured n times in the sample given out, will be added n times back, which is NOT how you want to do it most likely...
+        return sample_data, filter
 
     def add_samples(self, data, prio, time_stamps=None, filter=None):
         #Add time_stamps if needed, and filter the input if we are told to!
@@ -136,24 +91,27 @@ class experience_replay:
             data        = [       data[x] for x in filter]
             prio        = [       prio[x] for x in filter]
             time_stamps = [time_stamps[x] for x in filter]
-
         #Now that we have time-stamped and filtered data, we add it one sample at a time, keeping all deques sorted!
-        for i,d in enumerate(data):
-            if self.prioritized:
-                #What I think this does (and it seems to do) is:
-                # 1: find the first idx s.t. self.prio[idx] < prio[i]
-                # 2: insert entries e in deques s.t. e is and idx and subsequent entries are pushed to their previous index + 1
-                idx = bisect.bisect_left(self.prio, prio[i]) #Find index to insert
-                self.data.insert(idx, d      )
-                self.prio.insert(idx, prio[i])
-                self.time_stamps.insert(idx, time_stamps[i])
-            else:
-                self.data.append(d      )
-                self.prio.append(prio[i])
-                self.time_stamps.append(time_stamps[i])
+        i = 0
+        for d,p,t in zip(data,prio,time_stamps):
+            self.replace_sample(agent_utils.data_entry(d,p,t))
         if len(self) > self.max_size:
             self.remove_old( ceil(self.max_size*0.1) )
         self.time += len(data)
+
+    def replace_sample(self, entry):
+        if self.prioritized:
+            #What I think this does (and it seems to do) is:
+            # 1: find the first idx s.t. self.prio[idx] < prio[i]
+            # 2: insert entries e in deques s.t. e is and idx and subsequent entries are pushed to their previous index + 1
+            idx = bisect.bisect_left(self.data, entry) #Find index to insert
+            self.data.insert(idx, entry)
+        else:
+            self.data.append(entry)
+
+    def replace_samples(self, entries, filter):
+        for idx in filter:
+            self.replace_sample(entries[idx])
 
     def remove_old(self, cut_off):
         if self.log is not None:
@@ -166,35 +124,8 @@ class experience_replay:
                 del self.prio[idx]
                 del self.time_stamps[idx]
 
-    def merge(self, exp_rep):
-        def weave(x,y):
-            idx_x = idx_y = 0
-            ret = [None for _ in range(len(x)+len(y))]
-            for i in range(len(ret)):
-                if idx_x < len(x) and idx_y < len(y):
-                    pick_x = x[idx_x] < y[idx_y]
-                    ret[i] = x[idx_x] if pick_x else y[idx_y]
-                    if pick_x: idx_x += 1
-                    else: idx_y += 1
-                elif idx_x < len(x):
-                    ret[i] = x[idx_x]
-                    idx_x += 1
-                else:
-                    ret[i] = y[idx_y]
-                    idx_y += 1
-            return ret
-
-        a = deque((0 + 2*np.arange(10000000)).tolist())
-        b = deque((1 + 2*np.arange(10000000)).tolist())
-        import time
-        t = time.time()
-        # weave(a,b)
-        sorted(a+b)
-        print("weave: ",time.time()-t)
-        from heapq import merge
-        t = time.time()
-        list(merge(a,b))
-        print("merge:",time.time()-t);exit()
+    def merge_in(self, exp_rep, mode):
+        return list(merge(self.data,exp_rep.data))
 
     def __len__(self):
         return len(self.data)
