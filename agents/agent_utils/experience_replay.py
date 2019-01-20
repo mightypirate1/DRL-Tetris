2ce27c8f1165d3fb3f1954c5ea600da0790623a5
Martin Frisk <yfflan@gmail.com>
2019-01-20 14:37:34 +0100

    Rewrote experience replay to be a lot faster but a little less general. Trajectories now do 1-step TD-errors and outputs np-arrays

--- agents/agent_utils/experience_replay.py
+++ agents/agent_utils/experience_replay.py
@@ -1,98 +1,34 @@
 import numpy as np
-from math import ceil
-from heapq import merge
-import bisect
+import scipy.stats
 import aux.utils as utils
 from .. import agent_utils
-from agents.datatypes import replaybuffer_entry
 
-'''
-This class is made to store experiences (arbitrary data basically).
-It is a simple implementation of https://arxiv.org/abs/1511.05952 and hopefully
-correct and efficient :)
-
-You feed it lists with data and priorities.
-It creates a replaybuffer_entry type object that it keeps sorted based on
-priority.
-
-You can get a random sample (of n replaybuffer_entry objects) with
-get_random_sample(n). The data you put in is accessible in those entries:
-For replaybuffer_entry e:
-    e.data #Has the data
-    e.prio #Has the priority
-    e.is_weight #Has the importance sampling weight
-    e.time_stamp #Has the time-stamp
-Since e is a class instance; unless you did pickle your sample or something, you
-can update the priority of your sample, and that updates the "source" in the
-buffer.
-
-Most importand fcns to know:
-get_random_sample(n, remove=False)
-    gives out a random (with replacement!) sample of replaybuffer_entry objects.
-    (1) remove=True removes the samples entries from the buffer. In this mode,
-    get_random_sample returns 2 lists. The first is the samples, the second is a
-    filter, which is passed to the replace_samples function if you do want to
-    put them back.
-    (2) remove=False (default) leaves the samples in the buffer. As the samples
-    are of type replaybuffer_entry, you can
-
-add_samples(data,prio)
-    if you have collected fresh data from the env. use this to get it in the
-    buffer. If data is of arbitrary type, add it with priorities.
-
-replace_samples(data, filter)
-    If you got a sample from get_random_sample(n,remove=True) it came with a
-    filter. pass that sample (update the priority value first if you plan to do
-    that) together with the filter to replace them properly in the buffer. This
-    is because the sampling was done WITH REPLACEMENT, so the entries in the
-    sample is NOT UNIQUE. The filter keeps track of what entries needs to be
-    put back (and if you do something other than just updating stuff, you should
-    figure out how the filter works, and use it in your code to ensure
-    correctness).
-
-merge_in(another_buffer)
-    merges the data of another_buffer into this buffer, maintaining sorted
-    order.
-
-TO MAKE IT WORK IN A MULTI-PROCESS SETTING:
-The buffer keeps its own clock to keep track of what samples are to be removed.
-The remove-method of the class (that is used to regulate the size of the buffer)
-assumes that these indices are unique and in order. Thus merge_in is very
-distruptive since it just "pours in" new samples. The fix for this when used in
-a multi-worker/single-trainer setting is that the workers send data frequently
-enough so that they NEVER have to empty their own buffer EVER. On top of that,
-a time_fcn is passed to the constructor, which maps local time to a unique
-global time (see below). If one then synchs the local/global times with the
-set_time / get_time functions frequently enough, the outcome should be near
-perfect. Example time_fcn is lambda x : a*x+b where a is total number of
-workers, and b is worker id (assuming workers are numbered
-0,1,...,total_number_of_workers).
-'''
 class experience_replay:
-    def __init__(self, size, prioritized=True, log=None, time_fcn=lambda x:x):
-        self.max_size = size
-        self.log = log
-        self.data = list()
-        self.time = 0
-        self.time_fcn = time_fcn
-        self.time_increment = time_fcn(1)-time_fcn(0)
-        self.prioritized = prioritized
-        #
-        self.total_removed = 0  #Total entries removed ever
-        self.remove_count = 0.1 * self.max_size
+    def __init__(self, max_size=None, state_size=None, log=None):
+        self.log        = log
+        self.max_size   = max_size
+        self.state_size = state_size
+        self.states   = np.zeros((max_size,*state_size))
+        self.s_primes = np.zeros((max_size,*state_size))
+        self.rewards  = np.zeros((max_size,1))
+        self.dones    = np.zeros((max_size,1))
+        self.prios    = -np.ones((max_size,1))
+        self.current_size  = 0
+        self.current_idx   = 0
+        self.total_samples = 0
 
     def get_random_sample(self, n_samples, alpha=1.0, beta=1.0, remove=False):
         #Create the sampling distribution (see paper for details)
-        n = len(self)
+        n = self.current_size
+        # all_indices = np.arange(n)
         all_indices = np.arange(n)
-        if self.prioritized:
-            one_over_n = 1/n #Rank-based sampling
-            one_over_surpriserank = (1/(n-all_indices))
-            p_unnormalized = one_over_surpriserank**alpha
-        else:
-            p_unnormalized = np.ones((n,))
-            p = p_unnormalized / p_unnormalized.sum()
-            is_weights_all = np.ones((n,1))
+        sort_idxs = self.prios[:n].argsort(axis=0).ravel()
+
+        #make ranking
+        rank = 1+n-scipy.stats.rankdata(self.prios[:n].ravel(), method='ordinal')
+        #make a ranking based probability disribution (pareto-ish)
+        one_over_rank = 1/rank #Rank-based sampling
+        p_unnormalized = one_over_rank**alpha
         p = p_unnormalized / p_unnormalized.sum() #sampling distribution done
         is_weights_unnormalized = ((n*p)**(-beta))[:,np.newaxis] #Compute the importance sampling weights
         is_weights_all = is_weights_unnormalized/is_weights_unnormalized.reshape(-1).max()
@@ -102,90 +38,55 @@ class experience_replay:
         indices = np.random.choice(all_indices, size=n_samples, p=p).tolist()
 
         ##Data collection, and index-tracking
-        sample_data = [None]*n_samples
         for i, idx in enumerate(indices):
             idx_dict[idx]         = i #This makes sure that idx_dict keeps track of the position of the last occurance of each index in the sample
-            sample_data[i]        = self.data[idx]
-            sample_data[i].set_isw(is_weights_all[idx])
-        filter = list(idx_dict.values()) #The filter is a list containing the last occurance of each unique sample. This means, that if we add back as many samples as is in the filter, the number of samples in the buffer is unchanged.
+        filter = idx_dict #The filter is a list containing the last occurance of each unique sample. This means, that if we add back as many samples as is in the filter, the number of samples in the buffer is unchanged.
+        # filter = list(idx_dict.values()) #The filter is a list containing the last occurance of each unique sample. This means, that if we add back as many samples as is in the filter, the number of samples in the buffer is unchanged.
+        is_weights = is_weights_all[indices]
+        data = (
+                self.states[indices,:],
+                None,
+                self.rewards[indices,:],
+                self.s_primes[indices,:],
+                self.dones[indices,:],
+                )
 
-        #If the caller did not specify that they will not give the samples back (by setting remove=False), we assume they want to update priority etc. Since we do not have an update-prio fcn (this may be on the wish-list) we instead just remove the samples, and expect the caller to replace them later
-        if remove:
-            indices_to_be_removed = list(idx_dict.keys())
-            indices_to_be_removed.sort(reverse=True)
-            for idx in indices_to_be_removed:
-                del self.data[idx]
-            #We now give the sampled data to the caller.
-            #The filter is provided so that they can compute new priorities, update the data_entries, and put them back through replace_samples.
-            #IMPORTANT: pass the filter given here to the replace_samples method when replacing samples, otherwise any data that occured n times in the sample given out, will be added n times back, which is NOT how you want to do it most likely...
-        return sample_data, filter
+        return data, is_weights, filter
 
     def add_samples(self, data, prio):
-        #Add time_stamps if needed, and filter the input if we are told to!
-        time_stamps = [self.time_fcn(t) for t in range(self.time,self.time+len(data),1)]
-        if not self.prioritized:
-            prio = [None for _ in data]
-        for d,p,t in zip(data,prio,time_stamps):
-            if issubclass(type(d), replaybuffer_entry):
-                d.prio, d.time_stamp = p, t
-                self.replace_sample(d)
-            else:
-                self.replace_sample(replaybuffer_entry(d,p,t))
-        self.test_length()
-        self.time += self.time_increment * len(data)
+        s,_,r,sp,d = data
+        n = prio.size
+        idxs = [x%self.max_size for x in range(self.current_idx, self.current_idx+n)]
+        self.states[idxs,:]   = s
+        self.rewards[idxs,:]  = r
+        self.s_primes[idxs,:] = sp
+        self.dones[idxs,:]    = d
+        self.prios[idxs,:]    = prio
+        self.current_idx += n
+        # self.sort()
+        self.current_size = min(self.current_size+n, self.max_size)
+        self.total_samples += n
+    # def add_samples(self, data, prio):
+    #     s,_,r,sp,d = data
+    #     n = prio.size
+    #     self.states[-n:,:]   = s
+    #     self.rewards[-n:,:]  = r
+    #     self.s_primes[-n:,:] = sp
+    #     self.dones[-n:,:]    = d
+    #     self.prios[-n:,:]    = prio
+    #     # self.sort()
+    #     self.current_size = min(self.current_size+n, self.max_size)
 
-    def replace_sample(self, entry):
-        assert issubclass(type(entry), replaybuffer_entry), "replace_sample called with type:{}  -- replaybuffer_entry or subclass was expected"
-        if self.prioritized:
-            #What I think this does (and it seems to do) is:
-            # 1: find the first idx s.t. self.prio[idx] < prio[i]
-            # 2: insert entries e in deques s.t. e is and idx and subsequent entries are pushed to their previous index + 1
-            idx = bisect.bisect_left(self.data, entry) #Find index to insert
-            self.data.insert(idx, entry)
-        else:
-            self.data.append(entry)
+    def sort(self):
+        new_idxs = self.prios[:,0].argsort()
+        self.states[:,:] = self.states[new_idxs,:]
+        self.rewards[:,:] = self.rewards[new_idxs,:]
+        self.s_primes[:,:] = self.s_primes[new_idxs,:]
+        self.dones[:,:] = self.dones[new_idxs,:]
+        self.prios[:,:] = self.prios[new_idxs,:]
 
-    def replace_samples(self, entries, filter=None):
-        for idx in (filter if filter is not None else range(len(entries))):
-            self.replace_sample(entries[idx])
-
-    def remove_old(self):
-        threshold = self.total_removed + self.remove_count
-        count = 0
-        for i, d in reversed(list(enumerate(self.data))):
-            if d.time_stamp < threshold:
-                del self.data[i]
-                count += 1
-        self.total_removed += count
-        if self.log is not None:
-            self.log.debug("REMOVED {} OBJECTS ({} of total)".format(count,count/len(self)))
-
-    def merge_in(self, data):
-        if type(data) is experience_replay:
-            data = data.data
-        self.data = list(merge(self.data,data))
-        self.test_length()
-
-    def test_length(self):
-        if len(self) > self.max_size:
-            self.remove_old()
-
-    def reset(self):
-        self.__init__(prioritized=self.prioritized,log=self.log, time_fcn=self.time_fcn)
-
-    def clear_buffer(self):
-        self.total_removed += len(self)
-        self.data = list()
-
-    def set_time(self, t):
-        self.time = t
-
-    def get_time(self):
-        return self.total_removed + len(self) #This should count all elements that ever passed through the buffer
-
-    def print_out(self):
-        for d in self.data:
-            print(d)
+    def update_prios(self, new_prios, filter):
+        self.prios[list(filter.keys()),:] = new_prios[list(filter.values()),:]
 
     def __len__(self):
-        return len(self.data)
+        return self.current_size
--- agents/datatypes/trajectory.py
+++ agents/datatypes/trajectory.py
@@ -1,29 +1,16 @@
 import numpy as np
 from agents.datatypes import processed_trajectory
 
-DEBUG = False #<----------
-import time  #<----------
-
-
 class trajectory:
-    def __init__(self):
+    def __init__(self, state_fcn=None):
         self.length = 0
         self.s, self.r, self.d, self.p = [], [], [], []
-        self.max_len = 5
-
-
-
-        self.last_print  = 0 #<----------
-
-
-
 
     def get_states(self):
-        return [self.s[i] for i,a in enumerate(self.a) if a is not None]
+        return self.s
 
     def add(self,e, end_of_trajectory=False):
         state,action,reward,s_prime,player,done = e
-        if DEBUG: print(state,reward,player,done, "<- state,reward,player,done"); input()
         self.s.append(state)
         self.p.append(player)
         if not end_of_trajectory:
@@ -31,83 +18,19 @@ class trajectory:
             self.d.append(done)
             self.length += 1
 
-    def get_length(self): #TODO 1 should suffice!
-        return self.length
-    def __len__(self):
-        return self.get_length()
-
     def get_cumulative_reward(self, gamma_discount=0.999):
         return sum([x*gamma_discount**i for i,x in enumerate(self.r)])
 
-    def process_trajectory(self, model, gamma_discount=0.99, lambda_discount=0.9, player_adjusted=True, update=False):
-        _v,_ = model(self.s, player=self.p)
-        #Our default setting is that we invert the sign of every other value, since it represents the other players state...
+    def process_trajectory(self, model, state_fcn, gamma_discount=0.99):
+        v = model(self.s, player=self.p)
+        r = np.array(self.r).reshape((-1,1))
+        d = np.array(self.d).reshape((-1,1))
+        td_errors = -v[:-1] + r -gamma_discount * v[1:] * (1-d)
+        prios = np.abs(td_errors)
+        s  = state_fcn(self.s[:-1], player=self.p[:-1])
+        sp = state_fcn(self.s[1:], player=self.p[1:])
+        data = (s,None,r,sp,d)
+        return data, prios
 
-        # Raw data
-        v = np.array(_v).ravel()
-        r = np.array(self.r).ravel()
-        d = np.array(self.d).ravel().astype(np.int)
-        #To be filled in...
-        advantages = np.zeros(r.shape)
-        target_values = np.zeros(r.shape)
-
-        # ''' - - - - - '''                                             #<----------
-        # losetime = np.argmax(np.abs(r))                   #<----------
-        # v = np.arange(v.size)/v.size                  #<----------
-        # for i in range(losetime%2,v.size,2):                  #<----------
-        #     v[i] = -v[i]                  #<----------
-        # print(np.round(v,decimals=2))                 #<----------
-        # print(r)                  #<----------
-        # # exit()                  #<----------
-        # ''' - - - - - '''                 #<----------
-
-
-
-        #Compute the easy stuff!
-        td_errors = -v[:-1] + r - gamma_discount * v[1:] * (1 - d)
-        priorities = np.abs(td_errors)
-        if DEBUG: print(td_errors)
-
-        # ''' Multi-step advantages... maybe broken '''
-        # #This computes a truncated version of the GAE from https://arxiv.org/abs/1506.02438
-        # # but here we also adjust for the 2-player/1-policy setting by flipping sign of every 2nd advantage
-        # sum = 0.0
-        # LG = lambda_discount * gamma_discount
-        # # L,G = lambda_discount, gamma_discount
-        # weight_sum = 0.0
-        # for i in reversed(range(len(self))):
-        #     sum = sum * (-LG) + td_errors[i]
-        #     weight_sum = LG*weight_sum+1
-        #     advantages[i] = sum / weight_sum
-        #     if DEBUG: print("!:",advantages[i],sum,td_errors[i], LG)
-        # target_values = v[:-1] + advantages
-
-        #DEBUG-print
-        # if time.time() - 3 > self.last_print and type(self) is trajectory:
-        #     self.last_print = time.time()
-        #     for _s,_r,_d,_p,i in zip(self.s,self.r,self.d,self.p, range(len(self))):
-        #         print(_s,_r,_d,_p,i,":",np.round(v[i],decimals=2),"\t->",np.round(target_values[i])," (",np.round(td_errors[i]),")")
-
-        ''' 1-step avdantates... '''
-        target_values = v[:-1] + td_errors
-
-        if update: #TODO: This if statement smuggles out these numbers for updates in processed_trajectory objects. It would be nicer if the prior parts of this method was shared in a tidier way!
-            return target_values[0], priorities[0]
-
-        if DEBUG: print([(t,r,td,p) for t,r,td,p in zip(target_values,r,td_errors,self.p)],"<- target, reward, td_errs, player");input()
-
-        data = []
-        for i in range(self.length):
-            I = min(i+self.max_len, len(self)) #This makes sure that all processed pieces have a state-list that is one longer than the other data it holds (compare the row "td_errors = ...")
-            d = processed_trajectory.processed_trajectory(
-                                                          self.s[i:I+1],
-                                                          self.r[i:I  ],
-                                                          self.d[i:I  ],
-                                                          self.p[i:I+1],
-                                                          target_values[i],
-                                                          priorities[i],
-                                                         )
-            data.append(d)
-        return data, priorities
-    # def end_episode(self,sp,d):
-    #     self.add((sp, None, None, d), end_of_trajectory=True)
+    def __len__(self):
+        return self.length
