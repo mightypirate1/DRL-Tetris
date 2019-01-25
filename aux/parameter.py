95511f921a38ffb97b36e147d507b8e3dc6471c1
Martin Frisk <yfflan@gmail.com>
2019-01-25 13:56:33 +0100

    exp param is less flexible but not bugged

--- aux/parameter.py
+++ aux/parameter.py
@@ -18,12 +18,10 @@ class exp_parameter(parameter): #Very crude exp-decay :)
         self.__init_vars__()
     def __init_vars__(self):
         assert self.final_val is not None and self.time_horizon is not None, "exp_parameter requires final_val=x and time_horizon=y with valid x,y to be passed to its constructor."
-        self.offset = min(self.init_val, self.final_val, 0) - 1
-        self.a = self.init_val - self.offset
-        self.b = self.final_val - self.offset
-        self.decay = math.log(self.b/self.a)/self.time_horizon
+        self.decay = math.log(self.final_val/self.init_val)/self.time_horizon
+
     def get_value(self, t):
-        return min(max(self.a * math.exp(self.decay * t) + self.offset, self.final_val), self.init_val)
+        return math.exp(self.decay * t)
 
 class linear_parameter(parameter):
     def __init__(self, value, **kwargs):
