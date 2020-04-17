328ba291b9f9379997ebbab83151a0390cd3f29c
Martin Frisk <yfflan@gmail.com>
2020-04-17 16:36:55 +0200

    small fix

--- agents/datatypes/trajectory.py
+++ agents/datatypes/trajectory.py
@@ -4,7 +4,7 @@ class trajectory:
     def __init__(self):
         self.length = 0
         self.s, self.r, self.d, self.p = [], [], [], []
-
+        self.winner = None
     def get_states(self):
         return self.s
 
@@ -16,12 +16,15 @@ class trajectory:
             self.r.append(reward)
             self.d.append(done)
             self.length += 1
+        else:
+            self.winner = 1 - self.p[-1]
 
     def get_cumulative_reward(self, gamma_discount=0.999):
         return sum([x*gamma_discount**i for i,x in enumerate(self.r)])
 
     def process_trajectory(self, model, state_fcn, reward_shaper=None, gamma_discount=0.99):
-        r = self.r if reward_shaper is None else reward_shaper(self.r)
+        _r = self.r if reward_shaper is None else reward_shaper(self.r)
+        r = [x() for x in _r]
         v = model(self.s, player=self.p)
         r = np.array(     r).reshape((-1,1))
         d = np.array(self.d).reshape((-1,1))
@@ -30,6 +33,9 @@ class trajectory:
         s  = state_fcn(self.s[:-1], player=self.p[:-1])
         sp = state_fcn(self.s[1:], player=self.p[1:])
         data = (s, sp,None,r,d)
+        # print("---")
+        # for i in range(len(self)):
+        #     print("v", v[i],"td_errors", td_errors[i],"r", r[i], _r[i])
         return data, prios
 
     def get_winner(self): #This function assumes that the reward is done correctly, and corresponds to winning only
