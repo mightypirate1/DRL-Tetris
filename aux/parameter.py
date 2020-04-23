979362410336e6cb6dfffbf89a643f95ecdafe3f
Martin Frisk <yfflan@gmail.com>
2020-04-23 14:09:25 +0200

    added comparison for parameters

--- aux/parameter.py
+++ aux/parameter.py
@@ -11,6 +11,11 @@ class parameter:
         return self.get_value(t)
     def get_value(self, t):
         return self.init_val
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            if self.init_val == other.init_val and self.final_val == other.final_val and self.time_horizon == other.time_horizon:
+                return True
+        return False
     def __str__(self):
         return "{}(init:{}, final:{}, time_horizon:{})".format(type(self).__name__,self.init_val,self.final_val,self.time_horizon)
 
@@ -26,6 +31,11 @@ class exp_parameter(parameter): #Very crude exp-decay :)
         assert self.time_horizon is None, "exp_parameter uses parameters init base and decay (and min,max): f(t) = init*base**(-decay*t) clipped to range [min, max]"
     def get_value(self, t):
         return max(min(self.init_val * self.base**(-self.decay*t), self.max), self.min)
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            if self.init_val == other.init_val and self.final_val == other.final_val and self.min == other.min and self.max == other.max and self.decay == other.decay and self.base == other.base:
+                return True
+        return False
 
 class linear_parameter(parameter):
     def __init__(self, value, **kwargs):
