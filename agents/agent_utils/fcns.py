c0e108b8f7c35bdebfddf95bc90cd933f2122556
Martin Frisk <yfflan@gmail.com>
2020-04-29 22:20:08 +0200

    changed the indexing

--- agents/agent_utils/fcns.py
+++ agents/agent_utils/fcns.py
@@ -4,22 +4,18 @@ from numpy.lib.stride_tricks import as_strided
 def k_step_view(x, k):
     #reshapes shape (N, d1,d2,..,di) -> (n,k,d1,d2,...,di) with full data-sharing.
     #... basically (s0,s1,...,sn) -> ((s0,..,sk),(s1,...,s(k+1)),...(s(n-k),...,sn))
-    shape = (x.shape[0]-k, k, *x.shape[1:])
+    shape = (x.shape[0]-k+1, k, *x.shape[1:])
     strides = x.strides[0], *x.strides
     return as_strided(x, shape=shape, strides=strides)
 
 
 if __name__ == "__main__":
-    data = np.zeros((100,2,2,), dtype=np.int64)
-    w = k_step_view(data, 2)
+    data = np.zeros((100,10,), dtype=np.uint8)
+    w = k_step_view(data, 5)
     # exit()
     for i in range(100):
-        data[i,:,:] = i
-    for i in range(98):
-        print("d",data[i,:])
-        print("w",w[i,:])
-        print("---")
-    print("====")
-    print(data[-3,:])
-    print("====")
-    print(w[-1,:])
+        data[i,:] = i*1.2
+    print("---")
+    print(data[-10:,:])
+    print("---")
+    print(w[-3:,:])
