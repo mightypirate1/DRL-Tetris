163cd56632c32965ba1e46df7fbf807defc895ab
Martin Frisk <yfflan@gmail.com>
2020-04-30 15:33:10 +0200

    setup the experiment

--- thread_train.py
+++ thread_train.py
@@ -34,22 +34,22 @@ render = not docoptsettings["--no-rendering"]
 
 settings = {
             #Project
-            "run-id" : "SIXten-alpha0",
+            "run-id" : "SIXten-Q01-k1",
             # "render_simulation" : True
 
             #Train parameters
-            "n_step_value_estimates"    : 3,
-            "n_samples_each_update"     : 16192,
+            "n_step_value_estimates"    : 1,
+            "n_samples_each_update"     : 16384,
             "minibatch_size"            : 128,
-            "n_train_epochs_per_update" : 5,
+            "n_train_epochs_per_update" : 3,
             "time_to_reference_update"  : 4, #How after how many do_training calls do we update the reference-model?
             "value_lr"                  : exp_parameter(1e-3, base=10.0, decay=3/total_steps),
 
             #Exp-replay parameters
-            "prioritized_replay_alpha"  : constant_parameter(0.7),
-            "prioritized_replay_beta"   : linear_parameter(0.5, final_val=1.0, time_horizon=total_steps),
-            "experience_replay_size"    : 2*10**6,
-            "experience_replay_sample_mode" : 'rank',
+            "prioritized_replay_alpha"      : constant_parameter(0.6),
+            "prioritized_replay_beta"       : linear_parameter(0.4, final_val=1.0, time_horizon=total_steps),
+            "experience_replay_size"        : 2*10**6,
+            "experience_replay_sample_mode" : 'proportional',
 
             "alternating_models"        : False,
             "time_to_training"          : 10**3,
@@ -111,8 +111,8 @@ settings = {
             "valuenet_hidden_size" : 256,
             "nn_regularizer" : 0.001,
             "nn_output_activation" : None,
-            "optimizer" : tf.train.GradientDescentOptimizer,
-            # "optimizer" : tf.train.AdamOptimizer,
+            # "optimizer" : tf.train.GradientDescentOptimizer,
+            "optimizer" : tf.train.AdamOptimizer,
 
             #Misc.
             "render"            : render,
