858bce8d0409eb53863ebe4f016e573bcaf6bc88
Martin Frisk <yfflan@gmail.com>
2020-05-19 14:52:24 +0200

    splitting out some general nn functionality

--- agents/networks/__init__.py
+++ agents/networks/__init__.py
@@ -1,3 +1,4 @@
 from agents.networks.value_net import value_net
 from agents.networks.prio_vnet import prio_vnet
 from agents.networks.prio_qnet import prio_qnet
+# from agents.networks.network_utils import network_utils
