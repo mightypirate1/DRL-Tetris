de61b2ff20f3f8d006caf8c82aa678db640bf577
Martin Frisk <yfflan@gmail.com>
2020-05-21 12:07:07 +0200

    adding silver-arch: a resblock-based duelling Q-architecture

--- agents/networks/builders/q_nets.py
+++ agents/networks/builders/q_nets.py
@@ -16,74 +16,45 @@ from agents.networks.builders import build_blocks as blocks
 class q_net_silver(q_net_base):
     def Q_V_A(self, vec, vis, one_eyed_advantage=False):
         #1) Pad visuals
+        # TODO: Get more data into the stacks! (Height? )
         vis = [N.apply_visual_pad(v) for v in vis]
         #2) Pass visuals thru 1 res-block:
-        visual_stream_resblock_settings = {
-                                            'n_layers' : 3,
-                                            'n_filters' : 128,
-                                            'filter_size' : (3,3),
-                                            'strides' : (1,1),
-                                            'peepholes' : True,
-                                            'pools' : False,
-                                            'dropout' : 0.15,
-                                            'training' : self.training_tf,
-                                          }
-        hidden_vis = [blocks.residual_block(v, **visual_stream_resblock_settings) for v in vis]
+        hidden_vis = [blocks.residual_block(v, **self.resblock_settings["visual"]) for v in vis]
         #3) Make feature-planes out of vector-data and stack it on hidden stream:
         vec = [N.conv_shape_vector(v, hv.shape) for v,hv in zip(vec, hidden_vis)]
         visvec = [N.peephole_join(_vec,   _vis,   mode="concat") for _vec,_vis in zip(vec,hidden_vis)]
-
         #4) Another res-block before we split off in V- and A- streams.
-        joined_stream_resblock_settings = {
-                                            'n_layers' : 3,
-                                            'n_filters' : 128,
-                                            'filter_size' : (5,5),
-                                            'strides' : (1,1),
-                                            'peepholes' : True,
-                                            'pools' : False,
-                                            'dropout' : 0.15,
-                                            'training' : self.training_tf,
-                                          }
-        joined = [blocks.residual_block(v, **joined_stream_resblock_settings) for v in visvec]
-
-        #5) More res-blocks!
-        adv_stream_resblock_settings = {
-                                        'n_layers' : 3,
-                                        'n_filters' : 128,
-                                        'filter_size' : (3,3),
-                                        'strides' : (1,1),
-                                        'peepholes' : True,
-                                        'pools' : False,
-                                        'dropout' : 0.15,
-                                        'training' : self.training_tf,
-                                        }
-        #Add your vector-data to my processed joined stream to compute advantges
-        adv = blocks.residual_block(N.peephole_join(joined[0], vec[1]), **adv_stream_resblock_settings)
-        _A = blocks.keyboard_conv(adv, self.n_rotations, self.n_pieces)
-
-        #If we are not a worker, we want to compute values!
-        if not self.worker_only:
-            val_stream_resblock_settings = {
-                                             'n_layers' : 3,
-                                             'n_filters' : 256,
-                                             'filter_size' : (5,5),
-                                             'strides' : (1,1),
-                                             'peepholes' : True,
-                                             'pools' : True,
-                                             'dropout' : 0.15,
-                                             'training' : self.training_tf,
-                                           }
-            _V = blocks.residual_block(N.peephole_join(*joined), **val_stream_resblock_settings)
-            _V = self.settings["piece_advantage_range"] * N.normalize_advantages(_V, axis=1, piece_mask=self.used_pieces_mask_tf, n_used_pieces=self.n_used_pieces) #This works here too!
-        else:
-            _V = tf.constant([0.0], dtype=tf.float32)
-        V_qshaped = tf.reshape(_V,[-1,1,1,_V.shape.as_list()[-1]]) #Shape for Q-calc!
-
-        #Finnishing touch
-        A = self.settings["advantage_range"] * N.normalize_advantages(_A, separate_piece_values=self.settings["separate_piece_values"], mode=self.settings["advantage_type"], piece_mask=self.used_pieces_mask_tf, n_used_pieces=self.n_used_pieces)
-        Q = V_qshaped + A
+        joined = [blocks.residual_block(v, **self.resblock_settings["visvec"]) for v in visvec]
+        #5) Add your vector-data to my processed joined stream then compute advantges
+        # TODO: Make a little tiny resblock that gets just some data from the other screen and joins it here
+        _adv_stream = N.peephole_join(joined[0], vec[1])
+        adv_stream = blocks.residual_block(_adv_stream, **self.resblock_settings["adv_stream"])
+        _A = blocks.keyboard_conv(adv_stream, self.n_rotations, self.n_pieces)
+        #7) If we are not a worker, we want to compute values!
+        _V = tf.constant([0.0], dtype=tf.float32)
+        if not self.worker_only: #Worker's need only to compute an arg-max, so we don't need the value for them :)
+            _V = blocks.residual_block(N.peephole_join(*joined), **self.resblock_settings["val_stream"])
+            _V = N.pool_spatial_dims_until_singleton(_V, warning=True)
+            if self.settings["separate_piece_values"]: #Treat pieces like actions
+                _V = self.settings["piece_advantage_range"] * N.normalize_advantages(_V, apply_activation=True, axis=1, inplace=True, piece_mask=self.used_pieces_mask_tf, n_used_pieces=self.n_used_pieces)
+        #8) Finnishing touch
+        A = self.settings["advantage_range"] * N.normalize_advantages(_A, apply_activation=True, separate_piece_values=self.settings["separate_piece_values"], mode=self.settings["advantage_type"], piece_mask=self.used_pieces_mask_tf, n_used_pieces=self.n_used_pieces)
+        Q = _V + A
         V = N.q_to_v(Q, mask=self.used_pieces_mask_tf, n_pieces=self.n_pieces)
         return Q, V, A
+    def initialize_variables(self):
+        n = 8 if self.settings["separate_piece_values"] else 1
+        resb_default      = {'n_layers' : 3, 'n_filters' : 128,                                                                 'dropout' : 0.15, 'training' : self.training_tf}
+        val_resb_settings = {'n_layers' : 3, 'n_filters' : 1024, 'output_n_filters' : n, 'filter_size' : (5,5), 'pools' : True, 'dropout' : 0.15, 'training' : self.training_tf}
+        if "residual_block_settings" not in self.settings:
+                self.resblock_settings = {"visual": resb_default, "visvec": resb_default, "adv_stream" : resb_default, "val_stream": val_resb_settings,}
+                return
+        resb_default.update(self.settings["residual_block_settings"]["default"])
+        self.resblock_settings = {"visual": resb_default, "visvec": resb_default, "adv_stream" : resb_default, "val_stream": val_resb_settings,}
+        if "residual_block_settings" in self.settings:
+            for key in self.resblock_settings:
+                if key in self.settings["residual_block_settings"]:
+                    self.resblock_settings[key].update(self.settings["residual_block_settings"][key])
 
 class q_net_keyboard(q_net_base):
     def Q_V_A(self, vector, visuals):
@@ -105,7 +76,7 @@ class q_net_keyboard(q_net_base):
         V_qshaped = tf.reshape(V,[-1,1,1,V.shape.as_list()[-1]]) #Shape for Q-calc!
         A = self.advantage_range * A_kbd
         #4) Combine values and advantages to form the Q-fcn (Duelling-Q-style)
-        A = N.normalize_advantages(_A, separate_piece_values=self.settings["separate_piece_values"], mode=self.settings["advantage_type"], piece_mask=self.used_pieces_mask_tf)
+        A = N.normalize_advantages(_A, apply_activation=True, separate_piece_values=self.settings["separate_piece_values"], mode=self.settings["advantage_type"], piece_mask=self.used_pieces_mask_tf)
         Q = V_qshaped + A
         V = N.q_to_v(Q, mask=self.used_pieces_mask_tf, n_pieces=self.n_pieces)
         return Q, V, A
@@ -128,14 +99,14 @@ class q_net_vanilla(q_net_base):
                             x,
                             self.n_rotations * self.n_translations * self.n_pieces,
                             name='advantages_unshaped',
-                            activation=N.advantage_activation_sqrt,
+                            activation=None,
                             kernel_initializer=tf.contrib.layers.xavier_initializer(),
                             bias_initializer=tf.zeros_initializer(),
                            )
         _A = self.advantage_range * tf.reshape(_A, [-1, self.n_rotations, self.n_translations, self.n_pieces])
 
         #4) Combine values and advantages to form the Q-fcn (Duelling-Q-style)
-        A = N.normalize_advantages(_A, separate_piece_values=self.settings["separate_piece_values"], mode=self.settings["advantage_type"], piece_mask=self.used_pieces_mask_tf)
+        A = N.normalize_advantages(_A, apply_activation=True, separate_piece_values=self.settings["separate_piece_values"], mode=self.settings["advantage_type"], piece_mask=self.used_pieces_mask_tf)
         Q = V_qshaped + A
         V = N.q_to_v(Q, mask=self.used_pieces_mask_tf, n_pieces=self.n_pieces)
         return Q, V, A
