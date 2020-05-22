fa6fb7ec590a16c5555a3d55284a17e0ff2b021b
Martin Frisk <yfflan@gmail.com>
2020-05-22 11:42:35 +0200

    better settings for low compute contexts

--- experiments/sventon_silver.py
+++ experiments/sventon_silver.py
@@ -9,13 +9,15 @@ from aux.parameter import *
 settings = {
             #Project
             "run-id" : "SVENton-silver_alpha_0",
+            # "run-id" : "arch01-silver_slim0",
+            "description" : "A res-block based architecture. In theory it's good when you scale up :)",
             "state_processor_separate_piece" : True,
             "q_target_locked_for_other_actions" : False,
             "advantage_type" : "mean",#,"none",#"mean", #"max",
-            "old_state_dict" : False,
             "q_net_type" : "silver",
             "residual_block_settings" : {
                                             "default" : {
+                                                            "n_layers" : 3,
                                                             "n_filters" : 64,
                                                         },
                                         },
@@ -31,13 +33,11 @@ settings = {
             #Train parameters
             "gae_lambda"                : 0.92, #0.95 default
             "n_step_value_estimates"    : 17,
-            # "n_samples_each_update"     : 16384,
-            "n_samples_each_update"     : 128,
+            "n_samples_each_update"     : 16384,
             "minibatch_size"            : 32, #256, #128
             "n_train_epochs_per_update" : 1,
             "time_to_reference_update"  : 5, #How after how many do_training calls do we update the reference-model?
             "value_lr"                  : exp_parameter(1e-4, base=10.0, decay=2/total_steps),
-            # "n_samples_to_start_training" : 40000, #0
 
             #Exp-replay parameters
             "prioritized_replay_alpha"      : constant_parameter(0.7),
@@ -56,7 +56,7 @@ settings = {
             # "dithering_scheme"    : "adaptive_epsilon",
             # "epsilon"  : linear_parameter(10, final_val=0.0, time_horizon=total_steps),
             "dithering_scheme"    : "epsilon",
-            "epsilon"  : exp_parameter(1.0, base=10.0, decay=2/total_steps),
+            "epsilon"  :   exp_parameter(1.0, base=10.0, decay=2/total_steps),
             "optimistic_prios" : 0.0,
 
             #Rewards
@@ -73,6 +73,8 @@ settings = {
             # "pieces" : [0,6],
             # "game_size" : [22,10],
             "time_elapsed_each_action" : 400,
+            "old_state_dict" : False, #SVENton uses the new one
+
             #Types
             "env_vector_type"   : tetris_environment_vector,
             "env_type"          : tetris_environment,
@@ -97,30 +99,7 @@ settings = {
             #Misc.
             "render"            : True,
             "bar_null_moves"    : True,
-
-            #Value net:
-            "vectorencoder_n_hidden" : 1,
-            "vectorencoder_hidden_size" : 256,
-            "vectorencoder_output_size" : 32,
-            ###
-            "pad_visuals"      : True,
-            "visualencoder_n_convs" : 3,
-            "visualencoder_n_filters" : (64,64,128,),
-            "visualencoder_filter_sizes" : ((5,5),(5,3),(5,3),),
-            "visualencoder_poolings" : [3], #Pooling after layer numbers in this list
-            "visualencoder_dropout" : 0.15,
-            "peephole_convs"   : True,
-            "peephole_join_style"   : "add", #"concat"
-            "visualencoder_peepholes" : [0,1,2,3,4,5,6,7],
-            ##Kbd-vis
-            "kbd_vis_n_convs" : 3,
-            "kbd_vis_n_filters" : [128,128,128],
-            ##Kbd
-            "keyboard_n_convs" : 3,
-            "keyboard_n_filters" : (64,64,),
-            ###
-            "valuenet_n_hidden" : 2,
-            "valuenet_hidden_size" : 512,
+            "resblock_dropout" : 0.15,
             "nn_regularizer" : 0.0001,
             "nn_output_activation" : tf.nn.tanh,
             # "optimizer" : tf.train.GradientDescentOptimizer,
