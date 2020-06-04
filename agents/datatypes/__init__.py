72080dba5cf69427681b7ff5a29db05839a8d150
Martin Frisk <yfflan@gmail.com>
2020-06-04 16:00:25 +0200

    unified sventon_trajectories and added computation of advantages

--- agents/datatypes/__init__.py
+++ agents/datatypes/__init__.py
@@ -1,1 +1,1 @@
-from agents.datatypes.trajectory import trajectory, q_trajectory, ppo_trajectory
+from agents.datatypes.trajectory import trajectory, sventon_trajectory, ppo_trajectory, q_trajectory
