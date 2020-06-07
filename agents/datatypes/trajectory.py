81463926ef1a9095329dabb5c3e183e82b1bc1b0
Martin Frisk <yfflan@gmail.com>
2020-06-07 23:51:14 +0200

    unified trajectories. added fcn for computing advangages so that workers can do that. added prototypical data augmenter

--- agents/datatypes/trajectory.py
+++ agents/datatypes/trajectory.py
@@ -46,7 +46,7 @@ class trajectory:
 class sventon_trajectory(trajectory):
     def __init__(self, *args, **kwargs):
         trajectory.__init__(self,*args, **kwargs)
-    def process_trajectory(self, model, state_fcn, reward_shaper=None, compute_advantages=False, gamma_discount=0.99, gae_lambda=0.96, k_steps=1):
+    def process_trajectory(self, model, state_fcn, reward_shaper=None, compute_advantages=False, gamma_discount=0.99, gae_lambda=0.96, k_steps=1, augment=False):
         assert len(self) > 0, "dont process empty trajectories!"
         a_env, a_int_raw = list(zip(*self.a))
         r = self.r if reward_shaper is None else reward_shaper(self.r)
@@ -63,10 +63,36 @@ class sventon_trajectory(trajectory):
         a_int_np[:,0] = probabilities
         if compute_advantages:
             a_int_np[:,1:] = self.adv_and_targets(v_piece, v_mean, r[:,0], d[:,0], gamma=gamma_discount, gae_lambda=gae_lambda)
-        prios = 2*np.ones_like(r) #Very large prio :)
         a = (a_env_np, a_int_np)
+        prios = 2*np.ones_like(r) #Very large prio :)
         data = (s,a,r,d)
+        if augment:
+            data, prios = self.augment_data(data, prios, state_fcn)
         return data, prios
+
+    def augment_data(self, data, prios, state_fcn):
+        piece_swap = (1,0,3,2,4,5,6) # permutation mapping piece-idxs on their refrected counterpart (L -> J, S -> Z, and the rest on themselves)
+        s, a, r, d = data
+        a_env_np, a_int_np = a
+        # Mirrored states
+        s2 = state_fcn(self.s, player=self.p, mirrored=True)
+        # Mirrored actions
+        a_env2_np = a_env_np.copy()
+        a_env2_np[:,1] = 9-a_env2_np[:,1]
+        for i,p in enumerate(a_env_np[:,2]):
+            a_env2_np[i,2] = piece_swap[p]
+        a2 = (a_env2_np, a_int_np)
+        (vec1a,vec1b), (vis1a,vis1b), p1 = s
+        (vec2a,vec2b), (vis2a,vis2b), p2 = s2
+        f = lambda x,y : np.concatenate([x,y],axis=0)
+        S = [ [f(vec1a,vec2a), f(vec1b,vec2b)], [f(vis1a,vis2a), f(vis1b,vis2b)], f(p1,p2)]
+        A = [f(a[0],a2[0]), f(a[1],a2[1])]
+        R = f(r,r)
+        D = f(d,d)
+        data  = (S,A,R,D)
+        prios =  np.concatenate([prios, prios], axis=0)
+        return data, prios
+
     def adv_and_targets(self, v_mean, v_piece, r, d, gamma=0.98, gae_lambda=0.96):
         advantages = np.zeros(v_mean.shape)
         v_next = np.zeros(v_mean.shape)
@@ -81,38 +107,6 @@ class sventon_trajectory(trajectory):
             advantages[i] = (A + v_mean[i] - v_piece[i]) / W #Adjusts the advantage so that the specific piece affects the advantage at current time step, while all other time steps sees it as the average piece value
         targets = v_piece + advantages
         return np.concatenate([advantages[:,np.newaxis], targets[:,np.newaxis]], axis=1)
-    # def adv_and_targets2(self, v_mean, v_piece, r, d, gamma=0.98, gae_lambda=0.96):
-    #     advantages = np.zeros((len(self),1))
-    #     weight = 0.0
-    #     a_t = 0.0
-    #     v_tplus1 = 0.0
-    #     gl = gae_lambda * gamma
-    #     for i, v_t, vp_t, r_t in reversed(list(zip(range(len(self)),v_mean, v_piece,r))):
-    #         weight += 1.0
-    #         a_t += r_t + gamma * v_tplus1 - vp_t
-    #         advantages[i,0] = a_t / weight
-    #         a_t += vp_t - v_t
-    #         v_tplus1 = v_t
-    #         weight *= gae_lambda
-    #         a_t *= gl
-    #     targets = (v_piece[:,np.newaxis] + advantages)
-    #     return np.concatenate([advantages, targets], axis=1)
 
-#
-# class q_trajectory(trajectory):
-#     def __init__(self):
-#         super().__init__()
-#     def process_trajectory(self, model, state_fcn, reward_shaper=None, gamma_discount=0.99, k_steps=1):
-#         assert len(self) > 0, "dont process empty trajectories!"
-#         #Ready the data!
-#         _s = self.s + [self.s[-1]]
-#         _p = self.p + [self.p[-1]]
-#         a = np.array( self.a          ).reshape((-1,len(self.a[0])))
-#         r = self.r if reward_shaper is None else reward_shaper(self.r)
-#         r = np.array( [R() for R in r]).reshape((-1,1))
-#         d = np.array( self.d          ).reshape((-1,1))
-#         prios = 4*np.ones_like(r) #Very large prio :)
-#         s  = state_fcn(self.s, player=self.p)
-#         data = (s,a,r,d) # s[t,0,:] <- s_t, s[t,1,:] <- s_(t+1) etc
-#         return data, prios
+#backwards compat
 ppo_trajectory = q_trajectory = sventon_trajectory
