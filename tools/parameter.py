1eaf89ad855a1b5ff5913cdce4491f306ebb6c5b
Martin Frisk <yfflan@gmail.com>
2020-06-26 12:38:43 +0200

    paramaters now works with  parameters as parameters

--- tools/parameter.py
+++ tools/parameter.py
@@ -10,16 +10,20 @@ class parameter:
     def __eval__(self, t):
         return self.get_value(t)
     def __call__(self, t):
-        return max( min(self.get_value(t), self.max), self.min )
+        return max( min(self.get_value(t), self._eval(self.max,t)), self._eval(self.min,t) )
     def get_value(self, t):
-        return self.init_val
+        return self._eval(self.init_val, t)
+    def _eval(self,x,t):
+        if issubclass(type(x), parameter):
+            return x.get_value(t)
+        return x
     def __eq__(self, other):
         if isinstance(other, type(self)):
             if self.init_val == other.init_val and self.final_val == other.final_val and self.time_horizon == other.time_horizon:
                 return True
         return False
     def __str__(self):
-        return "{}(init:{}, final:{}, time_horizon:{}{})".format(type(self).__name__,self.init_val,self.final_val,self.time_horizon,self.__str2__())
+        return "{}(init:{}, final:{}, time_horizon:{}, min/max:{}/{}, {})".format(type(self).__name__,self.init_val,self.final_val,self.time_horizon,self.min,self.max,self.__str2__())
     def __str2__(self):
         return ""
     def __repr__(self):
@@ -34,7 +38,9 @@ class exp_parameter(parameter): #Very crude exp-decay :)
     def __init_vars__(self):
         assert self.time_horizon is None, "exp_parameter uses parameters init base and decay (and min,max): f(t) = init*base**(-decay*t) clipped to range [min, max]"
     def get_value(self, t):
-        return max(min(self.init_val * self.base**(-self.decay*t), self.max), self.min)
+        eval = lambda x : self._eval(x,t)
+        init, base, dec = eval(self.init_val), eval(self.base), eval(self.decay)
+        return init * base**(-dec*t)
     def __eq__(self, other):
         if isinstance(other, type(self)):
             if self.init_val == other.init_val and self.final_val == other.final_val and self.min == other.min and self.max == other.max and self.decay == other.decay and self.base == other.base:
@@ -50,7 +56,8 @@ class linear_parameter(parameter):
     def __init_vars__(self):
         pass
     def get_value(self,t):
+        eval = lambda x : self._eval(x,t)
         x = max( min(t,self.time_horizon), 0 ) / self.time_horizon
-        return x * self.final_val + (1-x) * self.init_val
+        return x * eval(self.final_val) + (1-x) * eval(self.init_val)
 
 constant_parameter = parameter
